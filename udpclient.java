f666cdd0b55583a326b9a1e83cade056a662fb18
Madison Brooks <brooksma@mail.gvsu.edu>
2017-10-02 13:20:09 -0400

    Added error checking and comments
    comments are for error checking and can be found at the bottom of the server code. I tried to comment everwhere I made changes although I missed a lot realizing that git will handle that.
    To do now is really just sending the file keeping in mind that datagram sockets only send one package at a time when you call send and only receive one packet at a time when you call that too.
    So it needs to be in a loop of 5 because that is our shifting window size.

--- udpclient.java
+++ udpclient.java
@@ -6,6 +6,49 @@ import java.nio.*;
 import java.nio.channels.*;
 
 class udpclient{
+
+//new
+public udpclient(){}
+
+      /*
+        Checks if valid ip
+        @return True if string is a valid ip, else false
+      */
+      public static boolean isValidIP(String ip){
+      	try{
+      	    if(ip == null || ip.isEmpty()){
+      		return false;
+      	    }
+
+      	    String[] ipArr = ip.split("\\.");
+      	    if( ipArr.length != 4 ){
+      		return false;
+      	    }
+
+      	    for(String numStr : ipArr){
+      		int num = Integer.parseInt(numStr);
+      		if(num < 0 || num > 255){
+      		    return false;
+      		}
+      	    }
+
+      	    if(ip.endsWith(".")){
+      		return false;
+      	    }
+
+      	    return true;
+
+      	} catch(NumberFormatException e){
+      	    return false; //means it wasn't a number
+      	}
+      }
+
+//endnew
+
+
+
+
+
 	public static void main(String args[]){
 		try{
 			DatagramChannel sc = DatagramChannel.open(); 
@@ -13,9 +56,47 @@ class udpclient{
 
 
 			Console cons = System.console();
-			String ipaddr = cons.readLine("Enter an IP Address: ");
-			String portnumS = cons.readLine("Enter a port number: ");
-			int portnum = Integer.parseInt(portnumS);
+			String ipStr = cons.readLine("Enter an IP Address: ");
+		//new
+			ipStr = "127.0.0.1"; // Default ip address
+      	    boolean valid = false;
+      	    while(valid == false){
+          		ipStr = cons.readLine("Enter target IP address: ");
+              	valid = isValidIP(ipStr.trim());
+          		if(!valid){
+          		    System.out.println("IP address " + ipStr + " is not valid.");
+          		    continue;
+          		} else{
+          		    valid = true;
+          		}
+      	    }
+	
+			String portStr = cons.readLine("Enter a port number: ");
+		
+			int portnum=9876;
+			Boolean portNotValid = false;
+            if(portStr.matches("^[0-9]*$")){
+            	portnum = Integer.parseInt(portStr);
+            	if(portnum<1024 || 49151<portnum){
+					portNotValid = true;
+				}
+            }else{
+            	portNotValid = true;
+            }
+			while(portNotValid){
+				System.out.println("INVALID PORT NUMBER!");
+				portStr = cons.readLine("Enter port number to listen on: ");
+				if(portStr.matches("^[0-9]*$")){
+            		 portnum = Integer.parseInt(portStr);
+            		if(1024<portnum && portnum<49151){
+						portNotValid = false;
+					}
+            	}else{
+            		portNotValid = true;
+            	}
+			}
+			System.out.println("Now using port number "+portnum);
+		//end new
 
 			Boolean again = true; 
 			while(again){
@@ -24,7 +105,7 @@ class udpclient{
 					again = false;				
 				}
 				ByteBuffer buf = ByteBuffer.wrap(m.getBytes());
-				sc.send(buf,new InetSocketAddress(ipaddr,portnum)); 
+				sc.send(buf,new InetSocketAddress(ipStr,portnum)); 
 			
 				ByteBuffer buffer = ByteBuffer.allocate(4096);
 				SocketAddress clientaddr = sc.receive(buffer); //reading the socket
@@ -37,4 +118,4 @@ class udpclient{
 			System.out.println("Got an IO Exception");
 			}
 		}
-	}
+}
--- udpserver.java
+++ udpserver.java
@@ -10,22 +10,82 @@
       public static void main(String args[]){
           try{
               Console cons = System.console();
-              String portnumS = cons.readLine("Enter a port number: ");
-              int portnum = Integer.parseInt(portnumS);
+              
+              //Madison New Code
+              String portStr = cons.readLine("Enter a port number: ");
+              //String portStr = "9876"; //defult
+              int portnum=9876; //defult
+              Boolean portNotValid = false;
+              if(portStr.matches("^[0-9]*$")){
+              		portnum = Integer.parseInt(portStr);
+              		if(portnum<1024 || 49151<portnum){
+			  			portNotValid = true;
+					}
+            	}else{
+            		portNotValid = true;
+            	}
+				while(portNotValid){
+					System.out.println("INVALID PORT NUMBER!");
+					portStr = cons.readLine("Enter port number to listen on: ");
+					if(portStr.matches("^[0-9]*$")){
+            			 portnum = Integer.parseInt(portStr);
+            			if(1024<portnum && portnum<49151){
+							portNotValid = false;
+						}
+            		}else{
+            				portNotValid = true;
+            		}
+				}
+				System.out.println("Now using port number "+portnum);
+              //end Madison new code
               DatagramChannel c = DatagramChannel.open();
               c.bind(new InetSocketAddress(portnum));
               while(true){
-                  ByteBuffer buffer = ByteBuffer.allocate(4096);
+                  ByteBuffer buffer = ByteBuffer.allocate(1024); //1024 is the most amount of data we can send at a time
                   SocketAddress clientaddr = c.receive(buffer); //reading the socket
                   String message = new String(buffer.array());  
   //returns a socket address of whoever sent the packet
                   System.out.println(message);
                   buffer.flip();
-                  c.send(buffer,clientaddr); //will this work
+                  c.send(buffer,clientaddr); //will this work = heck yeah!
               }
           }catch(IOException e){
               System.out.println("Got an IO Exception");
           }
   
       }
-  }
+}
+
+/*
+Specs for the project so we don't have to keep looking at the website:
+
+-UDP (datagram) sockets must be used.
+
+-The data in each packet (not including any header information you add) must be 1024 bytes or less.
+
+-You must implement a sliding window of 5 packets for your file transfer. Stop-and-wait protocols will not receive full credit. Specifically,you must not wait for all acks of a window to be recieved before moving the window and sending new data. (AKA make it slide)
+
+-The file transfer must be reliable (recover from any packet loss, packet corruption, packet duplication, and packet reordering).
+
+-The client and server should each print to the screen each time they send or recieve a packet.
+
+- Do not load the entire file you will be sending into memory at once. Instead, only read as far as you are currently sending. So, when sending the first 5 packets, only read the first 5 packets worth of data from the file. Read more of the file when you are ready to send it. Keep all data in memory until it is acknowledged. If you are resending data, you must not re-read it from the file
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+

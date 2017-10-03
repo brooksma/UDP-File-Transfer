28bf9cc5fa99fdc8979cf7eb1020500431572be1
Madison Brooks <brooksma@mail.gvsu.edu>
2017-10-03 09:13:23 -0400

    part 1
    Sending a file and first attempt at a sliding window set up.

--- udpclient.java
+++ udpclient.java
@@ -7,62 +7,53 @@ import java.nio.channels.*;
 
 class udpclient{
 
-//new
-public udpclient(){}
-
-      /*
-        Checks if valid ip
-        @return True if string is a valid ip, else false
-      */
-      public static boolean isValidIP(String ip){
-      	try{
-      	    if(ip == null || ip.isEmpty()){
-      		return false;
-      	    }
-
-      	    String[] ipArr = ip.split("\\.");
-      	    if( ipArr.length != 4 ){
-      		return false;
-      	    }
-
-      	    for(String numStr : ipArr){
-      		int num = Integer.parseInt(numStr);
-      		if(num < 0 || num > 255){
-      		    return false;
-      		}
-      	    }
-
-      	    if(ip.endsWith(".")){
-      		return false;
-      	    }
-
-      	    return true;
-
-      	} catch(NumberFormatException e){
-      	    return false; //means it wasn't a number
-      	}
-      }
-
-//endnew
-
-
-
-
+	public udpclient(){}
+	
+		  /*
+			Checks if valid ip
+			@return True if string is a valid ip, else false
+		  */
+		  public static boolean isValidIP(String ip){
+			  try{
+				  if(ip == null || ip.isEmpty()){
+				  return false;
+				  }
+	
+				  String[] ipArr = ip.split("\\.");
+				  if( ipArr.length != 4 ){
+				  return false;
+				  }
+	
+				  for(String numStr : ipArr){
+				  int num = Integer.parseInt(numStr);
+				  if(num < 0 || num > 255){
+					  return false;
+				  }
+				  }
+	
+				  if(ip.endsWith(".")){
+				  return false;
+				  }
+	
+				  return true;
+	
+			  } catch(NumberFormatException e){
+				  return false; //means it wasn't a number
+			  }
+		  }
 
 	public static void main(String args[]){
 		try{
-			DatagramChannel sc = DatagramChannel.open(); 
+			DatagramChannel dc = DatagramChannel.open(); 
 //changing the transport layer protocal
-
-
 			Console cons = System.console();
-			String ipStr = cons.readLine("Enter an IP Address: ");
-		//new
-			ipStr = "127.0.0.1"; // Default ip address
+
+			udpclient uc = new udpclient();
+      	    String ipStr = "127.0.0.1"; // Default ip address
       	    boolean valid = false;
       	    while(valid == false){
           		ipStr = cons.readLine("Enter target IP address: ");
-              	valid = isValidIP(ipStr.trim());
+              	valid = uc.isValidIP(ipStr.trim());
           		if(!valid){
           		    System.out.println("IP address " + ipStr + " is not valid.");
           		    continue;
@@ -70,52 +61,86 @@ public udpclient(){}
           		    valid = true;
           		}
       	    }
-	
-			String portStr = cons.readLine("Enter a port number: ");
-		
-			int portnum=9876;
-			Boolean portNotValid = false;
-            if(portStr.matches("^[0-9]*$")){
-            	portnum = Integer.parseInt(portStr);
-            	if(portnum<1024 || 49151<portnum){
+
+
+      	    String portStr = cons.readLine("Enter target port number: ");
+	/*Validating the port number*/
+              Boolean portNotValid = false; //checking to see if the port is valid
+		      int portInt=9876; //declaring portInt so the code works
+		      if(portStr.matches("^[0-9]*$")){
+		            portInt = Integer.parseInt(portStr);
+		            if(portInt<1024 || 49151<portInt){
 					portNotValid = true;
 				}
-            }else{
-            	portNotValid = true;
-            }
+		        }else{
+		        	portNotValid = true;
+		        }
 			while(portNotValid){
 				System.out.println("INVALID PORT NUMBER!");
 				portStr = cons.readLine("Enter port number to listen on: ");
 				if(portStr.matches("^[0-9]*$")){
-            		 portnum = Integer.parseInt(portStr);
-            		if(1024<portnum && portnum<49151){
+		        	portInt = Integer.parseInt(portStr);
+		        	if(1024<portInt && portInt<49151){
 						portNotValid = false;
-					}
-            	}else{
-            		portNotValid = true;
-            	}
+				}
+		       	}else{
+		        		portNotValid = true;
+		        	}
 			}
-			System.out.println("Now using port number "+portnum);
-		//end new
+			System.out.println("Now using port number " + portInt); //end of checking port number
+
+			int portnum = Integer.parseInt(portStr);
 
 			Boolean again = true; 
 			while(again){
-				String m = cons.readLine("Enter your message: ");
-				if(m.equals("exit")){
-					again = false;				
-				}
+					String m = cons.readLine("Enter a filename to transfer: ");
+					if(m.equals("exit")){
+						again = false;				
+					}	
 				ByteBuffer buf = ByteBuffer.wrap(m.getBytes());
-				sc.send(buf,new InetSocketAddress(ipStr,portnum)); 
-			
-				ByteBuffer buffer = ByteBuffer.allocate(4096);
-				SocketAddress clientaddr = sc.receive(buffer); //reading the socket
-				String message = new String(buffer.array());  
-				//returns a socket address of whoever sent the packet
-				System.out.println(message);
-				m = message;
+				dc.send(buf,new InetSocketAddress(ipStr,portnum)); //sending file name
+				//remember send only sends one at a time so this line above is fine
+				
+				
+				ByteBuffer buffer = ByteBuffer.allocate(1024); //our buffer can only be 1024
+				
+				File newfile = new File(m); //make an empty file with that file name
+				FileOutputStream outstream = new FileOutputStream(newfile);
+          		FileChannel fc = outstream.getChannel();
+          		
+				dc.receive(buffer); // this recive is just getting the file size and nothing else
+				String fileSizeStr = new String(buffer.array());
+				System.out.println(fileSizeStr);
+				
+				/*@TODO: All of the size code is commented out because I kept getting casting errors
+				 but we will need it so that the client knows when it has the whole file*/
+				
+				//Long fileSize = Long.parseLong(fileSizeStr);
+				//Long endSize = new Long(0);
+				
+				
+				
+				int currWindow = 0;
+				ByteBuffer buff = ByteBuffer.allocate(1024); //our buffer can only be 1024
+				//while(endSize < fileSize){ //loop the sliding window again until the file is read
+					currWindow=0; //start all over again with the window system
+					while(currWindow<6){
+						//@TODO knowing what packet number we just recived
+						buff.flip();
+		      		    fc.write(buff);
+		      		    buff.compact();
+						dc.receive(buff);
+						//@TODO: add awknowlagements in here (can we shift the window or do we wait?)
+						currWindow++;
+						//endSize = newfile.length();
+						//int endSize = Integer.parseInt(endSize);
+					}
+				
+				//}
 			}
 		}catch(IOException e){
 			System.out.println("Got an IO Exception");
 			}
 		}
-}
+	}
+	
--- udpserver.java
+++ udpserver.java
@@ -1,91 +1,107 @@
 /*Madison Brooks and Bailey Freund
-  October 2, 2017
-  CIS 457-20 */
-  import java.io.*;
-  import java.net.*;
-  import java.nio.*;
-  import java.nio.channels.*;
-  
-  class udpserver{
-      public static void main(String args[]){
-          try{
-              Console cons = System.console();
-              
-              //Madison New Code
-              String portStr = cons.readLine("Enter a port number: ");
-              //String portStr = "9876"; //defult
-              int portnum=9876; //defult
-              Boolean portNotValid = false;
-              if(portStr.matches("^[0-9]*$")){
-              		portnum = Integer.parseInt(portStr);
-              		if(portnum<1024 || 49151<portnum){
-			  			portNotValid = true;
-					}
-            	}else{
-            		portNotValid = true;
-            	}
-				while(portNotValid){
-					System.out.println("INVALID PORT NUMBER!");
-					portStr = cons.readLine("Enter port number to listen on: ");
-					if(portStr.matches("^[0-9]*$")){
-            			 portnum = Integer.parseInt(portStr);
-            			if(1024<portnum && portnum<49151){
-							portNotValid = false;
+October 2, 2017
+CIS 457-20 */
+import java.io.*;
+import java.net.*;
+import java.nio.*;
+import java.nio.channels.*;
+import java.io.FileOutputStream;
+import java.io.File;
+
+class udpserver{
+    
+    public udpserver(){} //empty constructor
+
+    public static void main(String args[]){
+        
+        try{
+            udpserver server = new udpserver();
+            System.out.println("Below is a list of available files: ");
+            File curDir = new File(".");
+            File[] filesList = curDir.listFiles();
+                for(File f : filesList){
+                        if(f.isFile()){
+                                System.out.println("\t"+f.getName());
+                        }
+                }
+
+
+            Console cons = System.console();
+            Boolean portNotValid = false;
+            String portStr = cons.readLine("Enter port number to listen on: ");
+            int portInt = 9876; //default port number - will be changed to whatever user inputs
+            if(portStr.matches("^[0-9]*$")){
+                    portInt = Integer.parseInt(portStr);
+                    if(portInt<1024 || 49151<portInt){
+                		portNotValid = true;
+                	}
+            }else{
+                    portNotValid = true;
+                }
+            while(portNotValid){
+                System.out.println("INVALID PORT NUMBER!");
+                portStr = cons.readLine("Enter port number to listen on: ");
+                if(portStr.matches("^[0-9]*$")){
+                	portInt = Integer.parseInt(portStr);
+                    if(1024<portInt && portInt<49151){
+                    	portNotValid = false;
+                    }
+                }else{
+                	portNotValid = true;
+                  }
+            }
+            System.out.println("Now using port number "+portInt);
+
+
+            DatagramChannel c = DatagramChannel.open();
+            c.bind(new InetSocketAddress(portInt));
+
+            
+            while(true){
+            	ByteBuffer buffer = ByteBuffer.allocate(1024); //our buffer can only be 1024
+            	SocketAddress clientaddr = c.receive(buffer); //reading in the file name b/c its the first thing being sent
+            	System.out.println("A client is requesting information");
+            	String fileStr = new String(buffer.array());
+            	fileStr = fileStr.trim(); // have to trim leading and trailing spaces due to making string from oversized buffer
+            	File f = new File(fileStr);
+            	String filename = f.getName();
+            	boolean fileExists = f.exists();
+            	if(fileExists){
+                	System.out.println("File exists");
+                
+                	long filesize = f.length();
+                	String sizeStr = Long.toString(filesize);
+                	ByteBuffer sizebuffer = ByteBuffer.wrap(sizeStr.getBytes());
+                	c.send(sizebuffer,clientaddr); 
+                	//this send matches up with the clients recive so that the client has the size of the file.
+                	
+                	FileInputStream instream = new FileInputStream(f);
+                	FileChannel fc = instream.getChannel();
+                	ByteBuffer buf = ByteBuffer.allocate(1024);
+                	int bytesread = fc.read(buf);
+                	//TODO: making sure a number is being send with the packet so we know which packet to resend if need be ect.
+                	
+                	int currWindow = 0;
+                	
+                	while(bytesread != -1){
+		            	currWindow=0; //start all over again with the window system
+						while(currWindow<6){
+							//@TODO knowing what packet number we just recived
+							buf.flip();
+				  		    c.send(buf, clientaddr);
+				  		    buf.compact();
+							bytesread = fc.read(buf);
+							//@TODO: add awknowlagements in here (can we shift the window or do we wait? resend?)
+							currWindow++;
+							//endSize = newfile.length();
+							//int endSize = Integer.parseInt(endSize);
 						}
-            		}else{
-            				portNotValid = true;
-            		}
-				}
-				System.out.println("Now using port number "+portnum);
-              //end Madison new code
-              DatagramChannel c = DatagramChannel.open();
-              c.bind(new InetSocketAddress(portnum));
-              while(true){
-                  ByteBuffer buffer = ByteBuffer.allocate(1024); //1024 is the most amount of data we can send at a time
-                  SocketAddress clientaddr = c.receive(buffer); //reading the socket
-                  String message = new String(buffer.array());  
-  //returns a socket address of whoever sent the packet
-                  System.out.println(message);
-                  buffer.flip();
-                  c.send(buffer,clientaddr); //will this work = heck yeah!
-              }
-          }catch(IOException e){
-              System.out.println("Got an IO Exception");
-          }
-  
-      }
+                	}
+                }
+             }
+        }catch(IOException e){
+            System.out.println("Got an IO Exception");
+        }
+
+    }
 }
-
-/*
-Specs for the project so we don't have to keep looking at the website:
-
--UDP (datagram) sockets must be used.
-
--The data in each packet (not including any header information you add) must be 1024 bytes or less.
-
--You must implement a sliding window of 5 packets for your file transfer. Stop-and-wait protocols will not receive full credit. Specifically,you must not wait for all acks of a window to be recieved before moving the window and sending new data. (AKA make it slide)
-
--The file transfer must be reliable (recover from any packet loss, packet corruption, packet duplication, and packet reordering).
-
--The client and server should each print to the screen each time they send or recieve a packet.
-
-- Do not load the entire file you will be sending into memory at once. Instead, only read as far as you are currently sending. So, when sending the first 5 packets, only read the first 5 packets worth of data from the file. Read more of the file when you are ready to send it. Keep all data in memory until it is acknowledged. If you are resending data, you must not re-read it from the file
-
-
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-

5f85825262930dfbc320af9e5d24509b58d50442
baileyfriend <freund.bailey@gmail.com>
2017-10-02 21:51:48 -0400

    Added user input checking

--- udpclient.java
+++ udpclient.java
@@ -6,25 +6,102 @@ import java.nio.*;
 import java.nio.channels.*;
 
 class udpclient{
+
+	public fileTransferClient(){}
+	
+		  /*
+			Checks if valid ip
+			@return True if string is a valid ip, else false
+		  */
+		  public static boolean isValidIP(String ip){
+			  try{
+				  if(ip == null || ip.isEmpty()){
+				  return false;
+				  }
+	
+				  String[] ipArr = ip.split("\\.");
+				  if( ipArr.length != 4 ){
+				  return false;
+				  }
+	
+				  for(String numStr : ipArr){
+				  int num = Integer.parseInt(numStr);
+				  if(num < 0 || num > 255){
+					  return false;
+				  }
+				  }
+	
+				  if(ip.endsWith(".")){
+				  return false;
+				  }
+	
+				  return true;
+	
+			  } catch(NumberFormatException e){
+				  return false; //means it wasn't a number
+			  }
+		  }
+
 	public static void main(String args[]){
 		try{
 			DatagramChannel sc = DatagramChannel.open(); 
 //changing the transport layer protocal
+Console cons = System.console();
 
+
+      	    String ipStr = "127.0.0.1"; // Default ip address
+      	    boolean valid = false;
+      	    while(valid == false){
+          		ipStr = cons.readLine("Enter target IP address: ");
+              valid = ftc.isValidIP(ipStr.trim());
+          		if(!valid){
+          		    System.out.println("IP address " + ipStr + " is not valid.");
+          		    continue;
+          		} else{
+          		    valid = true;
+          		}
+      	    }
+
+
+      	    String portStr = cons.readLine("Enter target port number: ");
+	/*Validating the port number*/
+                  Boolean portNotValid = false; //checking to see if the port is valid
+		      int portInt=9876; //declaring portInt so the code works
+		      if(portStr.matches("^[0-9]*$")){
+		            portInt = Integer.parseInt(portStr);
+		            if(portInt<1024 || 49151<portInt){
+					portNotValid = true;
+				}
+		        }else{
+		        	portNotValid = true;
+		        }
+			while(portNotValid){
+				System.out.println("INVALID PORT NUMBER!");
+				portStr = cons.readLine("Enter port number to listen on: ");
+				if(portStr.matches("^[0-9]*$")){
+		        	portInt = Integer.parseInt(portStr);
+		        	if(1024<portInt && portInt<49151){
+						portNotValid = false;
+				}
+		       	}else{
+		        		portNotValid = true;
+		        	}
+			}
+			System.out.println("Now using port number " + portInt); //end of checking port number
 
 			Console cons = System.console();
-			String ipaddr = cons.readLine("Enter an IP Address: ");
-			String portnumS = cons.readLine("Enter a port number: ");
-			int portnum = Integer.parseInt(portnumS);
+			String ipStr = cons.readLine("Enter an IP Address: ");
+			String portStr = cons.readLine("Enter a port number: ");
+			int portnum = Integer.parseInt(portStr);
 
 			Boolean again = true; 
 			while(again){
-				String m = cons.readLine("Enter your message: ");
+				String filename = cons.readLine("Enter a filename to transfer: ");
 				if(m.equals("exit")){
 					again = false;				
 				}
 				ByteBuffer buf = ByteBuffer.wrap(m.getBytes());
-				sc.send(buf,new InetSocketAddress(ipaddr,portnum)); 
+				sc.send(buf,new InetSocketAddress(ipStr,portnum)); 
 			
 				ByteBuffer buffer = ByteBuffer.allocate(4096);
 				SocketAddress clientaddr = sc.receive(buffer); //reading the socket
